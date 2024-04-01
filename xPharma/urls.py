9630d8c86f5aa87ccdf241a82ddcb3cd87e641c2
marcom966 <marcom962015@gmail.com>
2024-04-01 16:14:28 +0200

    first commit of the backend

---
+++ xPharma/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('xPharma/hello', views.say_hello);
+]
---
+++ xPharma/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def say_hello(request):
+    return HttpResponse('hello world')
+
+# Create your views here.
+#it's like the service in Angular
