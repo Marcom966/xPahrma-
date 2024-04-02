17520a332d37ce323e1a41a9fcb310cf8c1de2fd
marcom966 <marcom962015@gmail.com>
2024-04-02 16:40:22 +0200

    Costruendo metodi per recuperare dati dal frontend (potrebbe essere sbagliato)

--- database/views.py
+++ database/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
 
+def all_products(request):
+    if request.method=='GET':
+        product_list = Product.objects.all()
+    #print(product_list)
 # Create your views here.
