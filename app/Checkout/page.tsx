c04cc0aaa459d1507f593da8e5a714c94ac38677
marcom966 <marcom962015@gmail.com>
2024-04-03 22:37:45 +0200

    aggiunto formulario per informazioni di spedizione

--- app/Checkout/page.tsx
+++ app/Checkout/page.tsx
@@ -1,14 +1,30 @@
 "use client"
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
 import "./checkout.css";
+import Link from "next/link";
 
 function Checkout (){
+    const [val, setVal] = useState('');
+    const [val2, setVal2] = useState('');
+    const [val3, setVal3] = useState('');
+    const [val4, setVal4] = useState('');
     const searchParams = useSearchParams();
     const search = searchParams.get('name');
     const price = searchParams.get('price');
-    const route = useRouter();    
+    const change1 = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setVal(event.target.value);
+    }
+    const change2 = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setVal2(event.target.value);
+    }
+    const change3 = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setVal3(event.target.value);
+    }
+    const change4 = (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setVal4(event.target.value);
+    }
+
 
     return (
         <center>
@@ -17,7 +33,16 @@ function Checkout (){
                     Stai acquistando: {search}
                 </h1>
                 <p className="pCheckout">Totale: {price}€</p>
-                <button className="buttonCheckout" type="button" onClick={()=>route.push('/ThankYou')}>Acquista</button>
+                <h1 className="indirizzo">indirizzo di fatturazione:</h1>
+                <p className="inserisci">Via</p>
+                <input type="text" className="input1" placeholder="inserisci via" onChange={change1}></input>
+                <p className="inserisci2">N° civico</p>
+                <input type="text" className="input2" placeholder="inserisci numero civico" onChange={change2}></input>
+                <p className="inserisci3">CAP</p>
+                <input type="text" className="input3" placeholder="inserisci CAP" onChange={change3}></input>
+                <p className="inserisci4">Città</p>
+                <input type="text" className="input4" placeholder="inserisci Città" onChange={change4}></input>
+                <Link className="buttonCheckout" href={{pathname: './ThankYou', query: {Via: val, Ncivico: val2, CAP: val3, città: val4, prodotto: search}}}>Acquista</Link>
             </div>
         </center>
         );
--- app/ThankYou/page.jsx
+++ app/ThankYou/page.jsx
@@ -2,12 +2,28 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import './Thankyou.css';
+import { useSearchParams } from "next/navigation";
 
 const ThankYou = () =>{
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const Via = searchParams.get('Via');
+    const Ncivico = searchParams.get('Ncivico');
+    const CAP = searchParams.get('CAP');
+    const città = searchParams.get('città');
+    const prodotto = searchParams.get('prodotto');
+
+
     return(
         <div className="ThankYou">
             <h1 className="bigH1">Grazie per l'acquisto</h1>
+            <center>
+            <p className="acquisto">Hai acquistato: {prodotto}</p>
+            <p className="acquisto2">{Via}</p>
+            <p className="acquisto3">{Ncivico}</p>
+            <p className="acquisto4">{CAP}</p>
+            <p className="acquisto5">{città}</p>
+            </center>
             <button type="button" className="buttonThanks" id="button" onClick={()=>router.push('/')}>Torna alla homepage</button>
         </div>
     )
