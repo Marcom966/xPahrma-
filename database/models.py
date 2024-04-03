36f82070b6a3579b677cd6c6711f2b5d7e5f393f
marcom966 <marcom962015@gmail.com>
2024-04-03 20:16:42 +0200

    Aggiustati alcuni parametri

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     price = models.IntegerField('Price Field', max_length=4)
     description = models.CharField('description', max_length=120)
     owner=models.CharField('Owner Name', max_length=120)
-    availability = models.DurationField('disponibilità', max_length=200)
+    availability = models.IntegerField('disponibilità', max_length=3)
 
     def __str__(self):
         return self.name
